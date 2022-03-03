3048f4e633c9957a60f23ddadef9c5f155401f65
wesley <wesleybaadm@gmail.com>
2022-03-02 22:06:19 -0300

    Curso inicial de Python

---
+++ adivinhacao.py
@@ -0,0 +1,32 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+
+while (rodada <= total_de_tentativas):
+    print("Tentativa", rodada, "de", total_de_tentativas)
+    chute_str = input("Digite o seu numero: ")
+    print("Você digitou " , chute_str)
+    chute = int(chute_str)
+
+    acertou = numero_secreto == chute
+    chute_maior = chute > numero_secreto
+    chute_menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou")
+    else:
+        if (chute_maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif (chute_menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+
+    rodada = rodada + 1
+
+
+print("Fim do jogo")
+
+
