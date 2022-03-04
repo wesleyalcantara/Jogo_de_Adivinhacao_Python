396deba16259d2234805e08c238f26b1297e3816
wesley <wesleybaadm@gmail.com>
2022-03-03 22:25:58 -0300

    Laço feito com for

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,9 +6,37 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= total_de_tentativas):
+#********* Laço com while **********
+
+#while (rodada <= total_de_tentativas):
+#    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+#
+#    chute_str = input("Digite o seu numero: ")
+#    print("Você digitou " , chute_str)
+#    chute = int(chute_str)
+#
+#    acertou = numero_secreto == chute
+#    chute_maior = chute > numero_secreto
+#    chute_menor = chute < numero_secreto
+#
+#    if (acertou):
+#        print("Você acertou")
+#    else:
+#        if (chute_maior):
+#            print("Você errou! O seu chute foi maior do que o número secreto.")
+#        elif (chute_menor):
+#            print("Você errou! O seu chute foi menor do que o número secreto.")
+#
+#    rodada = rodada + 1
+
+#print("Fim do jogo")
+
+
+#********* Laço com for **********
+
+for rodada in range(1, total_de_tentativas):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    
+
     chute_str = input("Digite o seu numero: ")
     print("Você digitou " , chute_str)
     chute = int(chute_str)
@@ -25,9 +53,4 @@ while (rodada <= total_de_tentativas):
         elif (chute_menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
 
-    rodada = rodada + 1
-
-
 print("Fim do jogo")
-
-
